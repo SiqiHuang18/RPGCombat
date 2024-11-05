429c1ccad5c234ebb2cf014c3dd984da782f4997
siqihuang <sh5688@nyu.edu>
2024-11-05 21:43:46 +0800

    startup data assset

---
+++ Source/RPGCombat/Public/DataAssets/StartUpData/DataAsset_StartUpDataHero.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "DataAsset_StartUpDataHero.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGCOMBAT_API UDataAsset_StartUpDataHero : public UDataAsset_StartUpDataBase
+{
+	GENERATED_BODY()
+	
+};
